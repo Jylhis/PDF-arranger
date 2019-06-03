dee7bd290a936d64d59eb72497b82130b713d022
Jylhis <markus@jylhis.com>
2019-06-03 17:18:55 +0300

    Working remove and move

--- PDF Arranger/DocumentViewController.swift
+++ PDF Arranger/DocumentViewController.swift
@@ -15,7 +15,53 @@ class DocumentViewController: UIViewController {
     @IBOutlet weak var pdfView: PDFView!
     @IBOutlet weak var NavBar: UINavigationBar!
     
+    
     var document: UIDocument?
+    
+    
+    // TODO
+    @IBAction func RemovePressed(_ sender: UIBarButtonItem) {
+        let currentPage = self.pdfView.currentPage
+        let pageNumber = pdfView.document!.index(for: currentPage!)
+        let alert = UIAlertController(title: "Are you sure?", message: "Page number \(pageNumber)", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { action in
+            self.pdfView.document?.removePage(at: pageNumber)
+            //self.pdfView.render
+        }))
+        alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
+        
+        self.present(alert, animated: true)
+    }
+    
+    // TODO
+    @IBAction func MovePressed(_ sender: UIBarButtonItem) {
+        let currentPage = self.pdfView.currentPage
+        let pageNumber = pdfView.document!.index(for: currentPage!)
+        
+        //1. Create the alert controller.
+        let alert = UIAlertController(title: "Where?", message: "Where do you want to move this pge?", preferredStyle: .alert)
+        
+        //2. Add the text field. You can configure it however you need.
+        alert.addTextField { (textField) in
+            textField.keyboardType = .numberPad
+            textField.text = "\(pageNumber)"
+        }
+        
+        // 3. Grab the value from the text field, and print it when the user clicks OK.
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alert] (_) in
+            let textField = Int((alert?.textFields![0].text)!)// Force unwrapping because we know it exists.
+            self.pdfView.document?.insert(currentPage!, at: textField!)
+            //print("Text field: \(textField!.text)")
+        }))
+        
+        // 4. Present the alert.
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func PressedSave(_ sender: UIBarButtonItem) {
+        self.pdfView.document?.write(to: <#T##URL#>)
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
